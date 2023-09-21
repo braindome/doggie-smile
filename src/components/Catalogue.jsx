eadc93ac24ce7357d65b259d4cfd12c65ef3b2d8
Antonio Saracino <104855075+braindome@users.noreply.github.com>
2023-09-21 02:24:31 +0200

    API url added

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -1,7 +1,16 @@
-const Catalogue = (props) => {
-    return (
-        <div>Catalogue Page</div>
-    )
-}
+const dogApiUrl = "https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683";
 
-export default Catalogue
+const Catalogue = (props) => {
+  return <div>Catalogue Page</div>;
+};
+
+const getDogList = async () => {
+  const response = await fetch(dogApiUrl);
+  const data = await response.json();
+
+  console.log("Current catalogue: " + data.record);
+
+  return data.record;
+};
+
+export default Catalogue;
