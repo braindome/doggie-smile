5c47ff4f75880af31a238e0f84cca2c962611160
Antonio Saracino <antonio.saracino83@gmail.com>
2023-10-02 14:19:27 +0200

    Some styling added to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,39 @@
-import "./Catalogue.css"
+import "./Catalogue.css";
+import { Link } from "react-router-dom";
+import home_icon from "../assets/home-icon.svg";
+import search_icon from "../assets/magnifying-glass.svg";
 
-const Header = ({searchInput, onSearchInputChange, onSearch}) => {
-
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            onSearch();
-        }
+const Header = ({ searchInput, onSearchInputChange, onSearch }) => {
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onSearch();
     }
+  };
 
-    return (
-        <div className="header">
-            <h1>Catalogue</h1>
-            <input 
-                type="text" 
-                value={searchInput}
-                onChange={(e) => onSearchInputChange(e.target.value)}
-                onSearch={handleKeyPress}
-                placeholder="Search for string..."
-            />
-            <button onClick={onSearch}>Search</button>
+  return (
+    <div className="header">
+      <div className="left-section">
+        <Link to="/">
+          <button>
+            <img src={home_icon} alt="Home" />
+          </button>
+        </Link>
+      </div>
+      <h1>Catalogue</h1>
+      <div className="right-section">
+        <input
+          type="text"
+          value={searchInput}
+          onChange={(e) => onSearchInputChange(e.target.value)}
+          onSearch={handleKeyPress}
+          placeholder="Search for string..."
+        />
+        <button onClick={onSearch}>
+          <img src={search_icon} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Header
+export default Header;
