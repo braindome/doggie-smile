6692431e47cb212819ef38cf1283ea2ca9b1bb9f
Antonio Saracino <antonio.saracino83@gmail.com>
2023-10-05 21:33:10 +0200

    Dog list reset when searching an empty string, added search through moreproperties

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -13,24 +13,62 @@ const Catalogue = (props) => {
   const [dogCardVisible, setDogCardVisible] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const fetchedDogList = await getDogList();
-        setDogList(fetchedDogList);
-        console.log(fetchedDogList.filter(dog => dog.name === "Molly"))
-      } catch (error) {
-        console.error("Error fetching dog list:", error);
-      }
-    };
+    // const fetchData = async () => {
+    //   try {
+    //     const fetchedDogList = await getDogList();
+    //     setDogList(fetchedDogList);
+    //     console.log(fetchedDogList.filter(dog => dog.name === "Molly"))
+    //   } catch (error) {
+    //     console.error("Error fetching dog list:", error);
+    //   }
+    // };
 
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    try {
+      const fetchedDogList = await getDogList();
+      setDogList(fetchedDogList);
+      console.log(fetchedDogList.filter(dog => dog.name === "Molly"))
+    } catch (error) {
+      console.error("Error fetching dog list:", error);
+    }
+  };
+
+
   const handleSearch = () => {
     try {
+
+      if (searchInput === "") {
+        console.log("search input empty");
+        fetchData();
+        return;
+      }
+
+      // const filteredDogs = dogList.filter((dog) =>
+      //   dog.name.toLowerCase().includes(searchInput.toLowerCase())
+      // );
+
+      // const filteredDogs = dogList.filter((dog) => {
+      //   for (let prop in dog) {
+      //     if (dog[prop] && dog[prop]
+      //       .toString()
+      //       .toLowerCase()
+      //       .includes(searchInput.toLowerCase())) {
+      //         return true;
+      //       }
+      //   }
+      //   return false;
+      // });
+
+      // TODO: fix filtering through sex (male is contained in string female)
       const filteredDogs = dogList.filter((dog) =>
-        dog.name.toLowerCase().includes(searchInput.toLowerCase())
-      );
+      `${dog.name} ${dog.chipNumber} ${dog.sex} ${dog.age.toString()} ${dog.owner} ${dog.breed}`
+        .toLowerCase()
+        .includes(searchInput.toLowerCase())
+    );
+
       setDogList(filteredDogs);
     } catch (error) {
       console.error('Error searching dogs:', error);
